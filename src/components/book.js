3ced3978a6e2639e40d19524f20073e32c0d35b1
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-12-07 09:53:22 -0500

    Fix linter issues

--- src/components/book.js
+++ src/components/book.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Proptypes from 'prop-types';
 
 export default function Book({ id, title, author }) {
-    return (
-        <li id={id}>
-            <p className='book-title'>{title}</p>
-            <p className='book-author' >{author}</p>
-            <button className='remove-btn'>REMOVE</button>
-        </li>
-    );
-};
+  return (
+    <li id={id}>
+      <p className="book-title">{title}</p>
+      <p className="book-author">{author}</p>
+      <button type="submit" className="remove-btn">REMOVE</button>
+    </li>
+  );
+}
 
 Book.propTypes = {
-    id: Proptypes.string.isRequired,
-    title: Proptypes.string.isRequired,
-    author: Proptypes.string.isRequired,
-}
+  id: Proptypes.string.isRequired,
+  title: Proptypes.string.isRequired,
+  author: Proptypes.string.isRequired,
+};
--- src/components/displaybooks.js
+++ src/components/displaybooks.js
@@ -6,38 +6,39 @@ const books = [
   {
     id: 1,
     title: 'The Great Gatsby',
-    author: 'Scott Fitzgerald'
+    author: 'Scott Fitzgerald',
   },
   {
     id: 2,
     title: 'The catcher in the Rye',
-    author: 'J.D. Salinger'
+    author: 'J.D. Salinger',
   },
   {
     id: 3,
     title: 'To kill a mockingbird',
-    author: 'Harper Lee'
+    author: 'Harper Lee',
   },
   {
     id: 4,
     title: 'Walden',
-    author: 'H.D. Thoureau'
+    author: 'H.D. Thoureau',
   },
 ];
 
 export default function Books() {
   return (
-    <div id='book-page-container'>
+    <div id="book-page-container">
       <div id="books-container">
-        {books.map((book)=>(
-          <Book 
+        {books.map((book) => (
+          <Book
             id={book.id}
+            key={book.id}
             title={book.title}
             author={book.author}
-          />  
+          />
         ))}
       </div>
       <AddBook />
-    </div>  
+    </div>
   );
 }
